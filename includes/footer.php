46d957fa018f8490731a8cb873a433451ec2e827
Ahmed Shokeer <shokeer95@gmail.com>
2021-03-28 05:11:00 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                &copy; 2020 Online Course Registration | By : <a href="https://www.linkedin.com/in/ahmedshokeer/" target="_blank">ASHOKEER</a>
+            </div>
+        </div>
+    </div>
+</footer>
